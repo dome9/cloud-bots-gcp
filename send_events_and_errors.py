f765c17baf0fb5e72cc2bec2104572d7d76aec0f
Ravid Atias <ravida@checkpoint.com>
2019-07-08 12:05:15 +0300

    CR fixes 3

--- send_events_and_errors.py
+++ send_events_and_errors.py
@@ -1,26 +1,36 @@
 import os
 import json
 from sendgrid import SendGridAPIClient
-from flatten_dict import flatten, unflatten
 
 
 def sendEvent(output_message):
     print(f'{__file__} - send event output_message : {output_message}')
-    with open('email_message.json') as const_message_file:
-        message = json.load(const_message_file)
-        try:
-            message = flatten(message)
-            list(list(message.values())[0][0].values())[0][0]['email'] = os.getenv('OUTPUT_EMAIL')
-            list(message.values())[2][0]['value'] = json.dumps(output_message)
-            message = unflatten(message)
-        except Exception as e:
-            print(f'{__file__} - send event failed to parse message, error: : {e}')
+    message = {
+        'personalizations': [
+            {
+                'to': [
+                    {
+                        'email': os.getenv('OUTPUT_EMAIL')
+                    }
+                ],
+                'subject': 'GCP Remediation Output'
+            }
+        ],
+        'from': {
+            'email': 'GCP@cloudBots.com'
+        },
+        'content': [
+            {
+                'type': 'text/plain',
+                'value': json.dumps(output_message)
+            }
+        ]
+    }
 
     try:
         sg = SendGridAPIClient(os.getenv('SEND_GRID_API_CLIENT'))
         response = sg.send(message)
-        print(f'{__file__} - send event respone status : {response.status_code}')
+        print(f'{__file__} - send event response status : {response.status_code}')
 
     except Exception as e:
         print(f'{__file__} - send event failed, error: : {e}')
-

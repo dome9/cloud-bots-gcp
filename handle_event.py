41d299b2f43308c6438de2e3fb90e9eeb3c177fb
Ravid Atias <ravida@checkpoint.com>
2019-07-24 17:58:34 +0300

    * Add option to read bots and params from remediationAction finding field

--- handle_event.py
+++ handle_event.py
@@ -1,6 +1,7 @@
 import re
 import importlib
 MININAL_TAG_LENGTH = 2
+MININAL_ACTION_LENGTH = 1
 
 def get_data_from_message(message):
     data = {}
@@ -16,7 +17,35 @@ def get_data_from_message(message):
         data['entity_id'] = entity.get('id')
         data['entity_name'] = entity.get('name')
         data['region'] = entity.get('region')
+    if 'remediationActions' in message:
+        data['remediationActions'] = message['remediationActions']
     return data
+
+def get_bots_from_finding(compliance_tags, remediation_actions):
+    bots = []
+    # Check if any of the tags have AUTO: in them. If there's nothing to do at all, skip it.
+    auto_pattern = re.compile('AUTO:')
+    for tag in compliance_tags:
+        tag = tag.strip()  # Sometimes the tags come through with trailing or leading spaces.
+        # Check the tag to see if we have AUTO: in it
+        if auto_pattern.match(tag):
+            tag_pattern = tuple(tag.split(' '))
+            # The format is AUTO: bot_name param1 param2
+            if len(tag_pattern) < MININAL_TAG_LENGTH:
+                continue
+            tag, bot, *params = tag_pattern
+            bots.append([bot, params])
+
+    for action in remediation_actions:
+        action_pattern = tuple(action.split(' '))
+        # The format is bot_name param1 param2
+        if len(action_pattern) < MININAL_ACTION_LENGTH:
+            continue
+        bot, *params = action_pattern
+        bots.append((bot, params))
+
+    return bots
+
 
 
 def handle_event(message, message_output):
@@ -28,44 +57,35 @@ def handle_event(message, message_output):
         print(f'{__file__} - rule passed, no remediation needed')
         return False
 
-    auto_pattern = re.compile('AUTO:')
     compliance_tags = message_data.get('compliance_tags')
-    if not compliance_tags or not filter(auto_pattern.search, compliance_tags):  # Rule Doesnt have any 'AUTO:' tags'
+    remediation_actions = message_data.get('remediationActions')
+    message_output['Rules violations found'] = []
+    bots = get_bots_from_finding(compliance_tags, remediation_actions)
+    if not bots or not len(bots):
+        print(f'''{__file__} - Rule: {message_data.get('rule_name')} Doesnt have any bots to run. Skipping.''')
         return False
 
-    message_output['Rules violations found'] = []
-    for tag in compliance_tags:
-        print(f'{__file__} - process tag : {tag}')
-        tag = tag.strip()  # Sometimes the tags come through with trailing or leading spaces.
-        # Check the tag to see if we have AUTO: in it
-        pattern = re.compile('^AUTO:\s.+')
+    for bot_to_run in bots:
         bot_data = {}
-        if pattern.match(tag):
-            bot_data['Rule'] = message_data.get('rule_name')
-            bot_data['ID'] = message_data.get('entity_id')
-            bot_data['Name'] = message_data.get('entity_name')
-            bot_data['Remediation'] = tag
-            # Pull out only the bot verb to run as a function
-            # The format is AUTO: bot_name param1 param2
-            tag_pattern = tuple(tag.split(' '))
-            # Tag pattern should contain at least AUTO: and bot name
-            if len(tag_pattern) < MININAL_TAG_LENGTH:
-                bot_data['Empty Auto'] = 'tag. No bot was specified'
-                continue
+        bot_data['Rule'] = message_data.get('rule_name')
+        bot_data['ID'] = message_data.get('entity_id')
+        bot_data['Name'] = message_data.get('entity_name')
+        bot, params = bot_to_run
+        bot_data['Remediation'] = bot
+        print(f'''{__file__} - Bot name to execute: {bot}''')
 
-            tag, bot, *params = tag_pattern
-            try:
-                bot_module = importlib.import_module(''.join(['bots.', bot]), package=None)
-            except ImportError as e:
-                bot_data['Bot'] = f'{bot} is not a known bot. skipping - {e}'
-                continue
+        try:
+            bot_module = importlib.import_module(''.join(['bots.', bot]), package=None)
+        except ImportError as e:
+            bot_data['Bot'] = f'{bot} is not a known bot. skipping - {e}'
+            continue
 
-            try:  ## Run the bot
-                bot_msg = bot_module.run_action(project_id, message['rule'], message['entity'], params)
-                bot_data['Execution status'] = 'passed'
-            except Exception as e:
-                bot_msg = f'Error while executing function {bot}. Error: {e}'
-                bot_data['Execution status'] = 'failed'
-            bot_data['Bot message'] = bot_msg
-            message_output['Rules violations found'].append(bot_data.copy())
+        try:  ## Run the bot
+            bot_msg = bot_module.run_action(project_id, message['rule'], message['entity'], params)
+            bot_data['Execution status'] = 'passed'
+        except Exception as e:
+            bot_msg = f'Error while executing function {bot}. Error: {e}'
+            bot_data['Execution status'] = 'failed'
+        bot_data['Bot message'] = bot_msg
+        message_output['Rules violations found'].append(bot_data.copy())
     return True

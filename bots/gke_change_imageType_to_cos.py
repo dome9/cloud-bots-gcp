39029396df88487f5e4ceefa7bbf5f1e75e3db64
RavidAtias <38915315+RavidAtias@users.noreply.github.com>
2019-08-29 15:13:13 +0300

    New bot google kubernetes change image type (#8)
    New bot added - gke_change_imageType_to_cos

---
+++ bots/gke_change_imageType_to_cos.py
@@ -0,0 +1,25 @@
+from typing import Dict, List
+
+from google.cloud import container_v1
+from oauth2client.client import GoogleCredentials
+
+COS_IMAGE_TYPE = 'COS'
+
+
+def run_action(project_id: str, rule: str, entity: Dict, params: List) -> str:
+    print(f'{__file__} - run_action started')
+    zone = entity['region']
+    name = entity['name']
+    print(f'{__file__} - project_id : {project_id} - zone : {zone} kubernetes name : {name}')
+    credentials = GoogleCredentials.get_application_default()
+    gke_client = container_v1.ClusterManagerClient(credentials=credentials)
+    node_pools = entity.get('nodePools', [])
+
+    for node_pool in node_pools:
+        if node_pool.get('config', {}).get('imageType') != COS_IMAGE_TYPE:
+            node_pool_id = node_pool['name']
+            print(f'{__file__} - node pool name to change: - {node_pool_id}')
+            response = gke_client.update_node_pool(project_id, zone, name, node_pool_id, node_pool['version'], COS_IMAGE_TYPE)
+            print(f'{__file__} - response -{response}')
+
+    return 'All node pools image type changed to COS successfully'

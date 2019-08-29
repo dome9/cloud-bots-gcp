41f5b92d4fdfff17e63542618a98dc8feb53a9a0
Ravid Atias <ravida@checkpoint.com>
2019-08-29 15:04:21 +0300

    * new bot added - gke_change_imageType_to_cos

--- bots/gke_change_imageType_to_cos.py
+++ bots/gke_change_imageType_to_cos.py
@@ -1,10 +1,12 @@
+from typing import Dict, List
+
 from google.cloud import container_v1
 from oauth2client.client import GoogleCredentials
 
 COS_IMAGE_TYPE = 'COS'
 
 
-def run_action(project_id, rule, entity, params):
+def run_action(project_id: str, rule: str, entity: Dict, params: List) -> str:
     print(f'{__file__} - run_action started')
     zone = entity['region']
     name = entity['name']
@@ -12,14 +14,12 @@ def run_action(project_id, rule, entity, params):
     credentials = GoogleCredentials.get_application_default()
     gke_client = container_v1.ClusterManagerClient(credentials=credentials)
     node_pools = entity.get('nodePools', [])
-    try:
-        for node_pool in node_pools:
-            if node_pool.get('config', {}).get('imageType') != COS_IMAGE_TYPE:
-                node_pool_id = node_pool.get('name')
-                print(f'{__file__} - node pool name to change: - {node_pool_id}')
-                gke_client.update_node_pool(project_id, zone, name, node_pool_id, node_pool.get('version'), 'COS')
-        response = 'All node pools image type changed to COS successfully'
-    except Exception as e:
-        response = 'Error while changing imageType to COS'
-    print(f'{__file__} - response -{response}')
-    return f'{response}'
+
+    for node_pool in node_pools:
+        if node_pool.get('config', {}).get('imageType') != COS_IMAGE_TYPE:
+            node_pool_id = node_pool['name']
+            print(f'{__file__} - node pool name to change: - {node_pool_id}')
+            response = gke_client.update_node_pool(project_id, zone, name, node_pool_id, node_pool['version'], COS_IMAGE_TYPE)
+            print(f'{__file__} - response -{response}')
+
+    return response

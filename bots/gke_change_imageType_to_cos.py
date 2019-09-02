7fb4cc383b3b454c54f9627ba072a2dcbf1137d7
Ravid Atias <ravida@checkpoint.com>
2019-09-02 15:38:52 +0300

    * GKE sdk usage improvements

--- bots/gke_change_imageType_to_cos.py
+++ bots/gke_change_imageType_to_cos.py
@@ -1,8 +1,8 @@
 from typing import Dict, List
 
-from google.cloud import container_v1
-
+from googleapiclient import discovery
 COS_IMAGE_TYPE = 'COS'
+from oauth2client.client import GoogleCredentials
 
 
 def run_action(project_id: str, rule: str, entity: Dict, params: List) -> str:
@@ -10,14 +10,23 @@ def run_action(project_id: str, rule: str, entity: Dict, params: List) -> str:
     zone = entity['region']
     name = entity['name']
     print(f'{__file__} - project_id : {project_id} - zone : {zone} kubernetes name : {name}')
-    gke_client = container_v1.ClusterManagerClient()
+    credentials = GoogleCredentials.get_application_default()
+    service = discovery.build('container', 'v1', credentials=credentials)
     node_pools = entity.get('nodePools', [])
 
     for node_pool in node_pools:
         if node_pool.get('config', {}).get('imageType') != COS_IMAGE_TYPE:
             node_pool_id = node_pool['name']
+            name = f'projects/{project_id}/locations/{zone}/clusters/{name}/nodePools/{node_pool_id}'
+            update_node_pool_request_body = {
+                "imageType": "COS"
+            }
             print(f'{__file__} - node pool name to change: - {node_pool_id}')
-            response = gke_client.update_node_pool(project_id, zone, name, node_pool_id, node_pool['version'], COS_IMAGE_TYPE)
+            request = service.projects().locations().clusters().nodePools().update(name=name, body=update_node_pool_request_body)
+            response = request.execute()
+
             print(f'{__file__} - response -{response}')
 
     return 'All node pools image type changed to COS successfully'
+
+

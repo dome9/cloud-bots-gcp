eeb3e5f12325465a7075b266a15344d359c32ac2
grocamador <gonzalrocamador@gmail.com>
2020-11-23 18:38:31 +0100

    new-message

--- bots/flow_logs_enable.py
+++ bots/flow_logs_enable.py
@@ -1,5 +1,3 @@
-import json
-import googleapiclient.errors
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
@@ -7,27 +5,23 @@ def run_action(project_id, rule, entity, params):
     print(f'{__file__} - run_action started')
     credentials = GoogleCredentials.get_application_default()
     service = discovery.build('compute', 'v1', credentials=credentials)
-    
     region = entity.get('region')
     fingerprint = entity.get('fingerPrint')
     subnetwork = entity.get('name')
 
     print(f'{__file__} - project_id : {project_id} - region : {region} - subnetwork : {subnetwork} - fingerprint : {fingerprint}')
     print("hola")
-    # Without fingerprint
-    # subnetwork_body = '''{ "logConfig": { "enable": true } '''
-
-    subnetwork_body = {"enableFlowLogs": "true", "fingerprint": fingerprint}
     
+
+    subnetwork_body = {
+        "enableFlowLogs": "true",
+        "fingerprint": fingerprint
+        }
     print("La variable funciona")
-
     print(subnetwork_body)
-
-    try:
-        request = service.subnetworks().patch(project=project, region=region, subnetwork=subnetwork, body=subnetwork_body)
-    except Error, InvalidJsonError, UnknownFileType as e:
-        print('Error',e)
+    request = service.subnetworks().patch(project=project_id, region=region, subnetwork=subnetwork, body=subnetwork_body)
     print(request)
+    print("request vacio")
     response = request.execute()
     print(f'{__file__} - response - {response}')
     return f'{response}'

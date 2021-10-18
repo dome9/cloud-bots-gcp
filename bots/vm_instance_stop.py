b3baa441e0260568ea068553a0d2004c544a15ef
yairshp-point <73879136+yairshp-point@users.noreply.github.com>
2021-10-18 11:10:42 +0300

    fixed error handling for vm_instance_stop

--- bots/vm_instance_stop.py
+++ bots/vm_instance_stop.py
@@ -2,6 +2,9 @@ from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 import logging
 
+ERRORS = 'errors'
+ERROR = 'error'
+
 
 def run_action(project_id, rule, entity, params):
     logging.info(f'{__file__} - run_action started')
@@ -14,24 +17,19 @@ def run_action(project_id, rule, entity, params):
     output_msg = ''
 
     try:
-        logging.info(f'__file__ - stopping virtual machine: {instance}')
+        logging.info(f'{__file__} - stopping virtual machine: {instance}')
         request = service.instances().stop(project=project_id, zone=zone, instance=instance)
         response = request.execute()
-        if response['error']:
-            msg = f'Failed stopping virtual machine - {instance}'
-            logging.info(f'__file__ - {msg}')
+        if ERROR in response:  # on failure
+            msg = f'Failed stopping virtual machine - {instance}: {response[ERROR]}'
+            logging.error(f'{__file__} - {msg}')
             output_msg += msg
-            # print all errors that occurred while trying to perform the action
-            for error in response['error']['errors']:
-                error_msg = str(error)
-                logging.info(f'__file__ - {error_msg}')
-                output_msg += error_msg
         else:  # on success
             msg = f'Virtual machine - {instance} was stopped'
-            logging.info(f'__file__ - {msg}')
+            logging.info(f'{__file__} - {msg}')
             output_msg += msg
     except Exception as e:
         msg = f'Unexpected error occurred - {e}'
-        logging.error(f'__file__ - {msg}')
+        logging.error(f'{__file__} - {msg}')
         output_msg += msg
     return output_msg

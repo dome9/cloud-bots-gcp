74626d0777d8314d1905ea3e860cff216c3b6afc
omerar <omerar@checkpoint.com>
2023-05-08 09:52:43 +0300

    changes

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from handle_event import *
 from send_events_and_errors import *
 from send_logs import *
+from send_logs_api_gateway import *
 
 
 def main(req):
     print('GCP cloud bot function processed a request.')
-    try:
-        source_message = req.get_json()
-    except Exception as e:
-        print('Bad request.')
-        return f'GCP cloud bot had an error'
+
+    source_message = req
 
     start_time = time.time()
     print(f'{__file__} - source message : {source_message}')
@@ -18,7 +16,14 @@ def main(req):
         print(f'source message : {source_message}')
         output_message['ReportTime'] = source_message.get('reportTime', 'N.A')
         output_message['Account id'] = source_message['account'].get('id', 'N.A')
-        output_message['Finding key'] = source_message.get('findingKey', 'N.A')
+        output_message['findingKey'] = source_message.get('findingKey', 'N.A')
+        output_message['logsHttpEndpoint'] = source_message.get('logsHttpEndpoint')
+        output_message['logsHttpEndpointKey'] = source_message.get('logsHttpEndpointKey')
+        output_message['logsHttpEndpointStreamName'] = source_message.get('logsHttpEndpointStreamName')
+        output_message['logsHttpEndpointStreamPartitionKey'] = source_message.get('logsHttpEndpointStreamPartitionKey')
+        output_message['dome9AccountId'] = source_message.get('dome9AccountId')
+        output_message['executionId'] = source_message.get('executionId')
+        output_message['vendor'] = source_message.get('account').get('vendor')
         try:
             export_results = handle_event(source_message, output_message)
         except Exception as e:
@@ -32,6 +37,7 @@ def main(req):
             print(f'''{__file__} - Output was not sent : {output_message}''')
         is_send_logs = os.getenv('SEND_LOGS', False)
         print(f'{__file__} - SEND_LOGS set to {str(is_send_logs)}')
+        send_logs_api_gateway(output_message);
         if is_send_logs:
             send_logs(output_message, start_time, source_message.get('account').get('vendor'))
     if output_message:

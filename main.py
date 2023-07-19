baff2ecb3ef89174c7fde1a3250145200bfa3a3b
Yael Eliaz <yaelel@checkpoint.com>
2023-07-19 15:05:20 +0300

    Int 3097 gcp remediation poc for function authentication (#50)
    * changes
    
    * changes
    
    * fixed bots
    
    * initial commit
    
    * removed versions fromrequirements.txt
    
    * .
    
    * .
    
    ---------
    
    Co-authored-by: omerar <omerar@checkpoint.com>

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import time
+import logging
 from handle_event import *
 from send_events_and_errors import *
 from send_logs import *
 from send_logs_api_gateway import *
 
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 def main(req):
-    print('GCP cloud bot function processed a request.')
+    logger.info('GCP cloud bot function processed a request.')
     source_message = req
-
     start_time = time.time()
-    print(f'{__file__} - source message : {source_message}')
+    logger.info(f'{__file__} - source message : {source_message}')
     output_message = {}
     if source_message:
-        print(f'source message : {source_message}')
+        logger.info(f'source message : {source_message}')
         output_message['ReportTime'] = source_message.get('reportTime', 'N.A')
         output_message['Account id'] = source_message['account'].get('id', 'N.A')
         output_message['findingKey'] = source_message.get('findingKey', 'N.A')
@@ -28,20 +31,19 @@ def main(req):
             export_results = handle_event(source_message, output_message)
         except Exception as e:
             export_results = True
-            print(f'{__file__} - Handle event failed')
+            logger.exception(f'{__file__} - Handle event failed: {str(e)}')
             output_message['Handle event failed'] = str(e)
         if export_results:
             if os.getenv('OUTPUT_EMAIL'):
                 sendEvent(output_message)
         else:
-            print(f'''{__file__} - Output was not sent : {output_message}''')
+            logger.info(f'{__file__} - Output was not sent: {output_message}')
         is_send_logs = os.getenv('SEND_LOGS', False)
-        print(f'{__file__} - SEND_LOGS set to {str(is_send_logs)}')
+        logger.info(f'{__file__} - SEND_LOGS set to {str(is_send_logs)}')
         send_logs_api_gateway(output_message)
         if is_send_logs:
             send_logs(output_message, start_time, source_message.get('account').get('vendor'))
     if output_message:
         return f'{output_message}'
-
     else:
         return f'GCP cloud bot had an error - {output_message}'
